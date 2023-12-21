c8ffe11197416ba77ebd88e8cd8836775c54c195
Paradox60 <Ferdinantte@gmail.com>
2023-12-21 21:09:16 +0200

    Hellow world start

--- webapp/main_app/urls.py
+++ webapp/main_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, page1, page2, page3
 
 urlpatterns = [
    path('', home, name="home"),
+   path('add_words/', page1, name='add_words'),
+   path('test/', page2, name='test'),
+   path('library/', page3, name='library'),
 ]
--- webapp/main_app/views.py
+++ webapp/main_app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render (request, 'main_app/home.html')
+
+def page1(request):
+    return render(request, 'main_app/add_words.html')
+
+def page2(request):
+    return render(request, 'main_app/test.html')
+
+def page3(request):
+    return render(request, 'main_app/library.html')
